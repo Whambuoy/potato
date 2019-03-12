b6f864b4a0176831397acbe8b891905b4f5c1d39
Whambuoy <esther25maina@gmail.com>
2019-03-12 20:54:35 +0300

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class = "row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">
+                            Product Name
+                        </div>
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+                    </div>
+                </div>                        
+            </div>                        
+        </div>                        
+    </div>
+
+@endsection
